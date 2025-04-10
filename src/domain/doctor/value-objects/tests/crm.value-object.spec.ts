b35d0f81f0554094d4b7fad8b480d2ae58d7fbbc
Lucas Bastos <lucsbasto@gmail.com>
2025-04-10 20:23:23 -0300

    test: ensure CRM creates a valid CRM with lowercase state and trim spaces

--- src/domain/doctor/value-objects/tests/crm.value-object.spec.ts
+++ src/domain/doctor/value-objects/tests/crm.value-object.spec.ts
@@ -7,4 +7,10 @@ describe('CRM', () => {
     const crm = CRM.create(crmValue);
     expect(crm.value).toBe(crmValue);
   });
+
+  it('should create a valid CRM with lowercase state and trim spaces', () => {
+    const crmValue = '  sp 987654 ';
+    const crm = CRM.create(crmValue);
+    expect(crm.value).toBe('SP987654');
+  });
 });
