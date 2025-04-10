63d4429d45ed703c8c10ffeec9a68753f025d5c9
Lucas Bastos <lucsbasto@gmail.com>
2025-04-10 20:39:27 -0300

    fix: ensure Specialty value is trimmed and converted to uppercase upon creation

--- src/domain/doctor/value-objects/tests/specialty.value-object.spec.ts
+++ src/domain/doctor/value-objects/tests/specialty.value-object.spec.ts
@@ -4,13 +4,13 @@ describe('Specialty', () => {
   it('should create a valid Specialty', () => {
     const specialtyValue = 'Cardiology';
     const specialty = Specialty.create(specialtyValue);
-    expect(specialty.value).toBe('Cardiology');
+    expect(specialty.value).toBe(specialtyValue.trim().toUpperCase());
   });
 
   it('should create a valid Specialty and trim leading/trailing spaces', () => {
     const specialtyValue = '  Dermatology  ';
     const specialty = Specialty.create(specialtyValue);
-    expect(specialty.value).toBe('Dermatology');
+    expect(specialty.value).toBe(specialtyValue.trim().toUpperCase());
   });
 
   it('should throw an error if the provided value is empty', () => {
