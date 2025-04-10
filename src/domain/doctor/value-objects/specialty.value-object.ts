628f9a400d87cd5fde51c0cd4459c0f273f94795
Lucas Bastos <lucsbasto@gmail.com>
2025-04-10 20:26:38 -0300

    feat: implement Specialty value object with validation for non-empty values

---
+++ src/domain/doctor/value-objects/specialty.value-object.ts
@@ -0,0 +1,22 @@
+import { ValueObject } from '@/common/domain/value-object/value-object';
+
+interface SpecialtyProps {
+  value: string;
+}
+
+export class Specialty extends ValueObject<SpecialtyProps> {
+  get value(): string {
+    return this.props.value;
+  }
+
+  private constructor(props: SpecialtyProps) {
+    super(props);
+  }
+
+  public static create(value: string): Specialty {
+    if (!value || value.trim() === '') {
+      throw new Error('Specialty cannot be empty.');
+    }
+    return new Specialty({ value: value.trim() });
+  }
+}
