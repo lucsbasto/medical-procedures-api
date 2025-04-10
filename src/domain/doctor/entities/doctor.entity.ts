f7cf4a34ff0a4b998ef1ec8f822f96eb537b30f8
Lucas Bastos <lucsbasto@gmail.com>
2025-04-10 20:39:36 -0300

    feat: implement Doctor entity with CRM and Specialty value objects

---
+++ src/domain/doctor/entities/doctor.entity.ts
@@ -0,0 +1,33 @@
+import { CRM } from '../value-objects/crm.value-object';
+import { Specialty } from '../value-objects/specialty.value-object';
+
+export class Doctor {
+  private _id: string;
+  private _name: string;
+  private _crm: CRM;
+  private _specialty: Specialty;
+
+  constructor(id: string, name: string, crmValue: string, specialtyValue: string) {
+    this._id = id;
+    this._name = name;
+    this._crm = CRM.create(crmValue);
+    this._specialty = Specialty.create(specialtyValue);
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get crm(): string {
+    return this._crm.value;
+  }
+
+  get specialty(): string {
+    return this._specialty.value;
+  }
+
+}
