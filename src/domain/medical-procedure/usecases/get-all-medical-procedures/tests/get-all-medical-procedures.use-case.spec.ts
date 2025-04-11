a72161184ff0474ddd24498c4b1eecfd7c5187eb
Lucas Bastos <lucsbasto@gmail.com>
2025-04-11 17:13:02 -0300

    test: add filtering by doctorId in GetAllMedicalProcedures use case

--- src/domain/medical-procedure/usecases/get-all-medical-procedures/tests/get-all-medical-procedures.use-case.spec.ts
+++ src/domain/medical-procedure/usecases/get-all-medical-procedures/tests/get-all-medical-procedures.use-case.spec.ts
@@ -1,6 +1,7 @@
 import { MedicalProcedure } from '@/domain/medical-procedure/entities/medical-procedure.entity';
 import { PaymentStatus } from '@/domain/medical-procedure/enums/payment-status.enum';
 import { MedicalProcedureRepository } from '@/domain/medical-procedure/repositories/medical-procedure.repository';
+import { GetAllMedicalProceduresInputDto } from '../../dtos/get-all-medical-procedures-input.dto';
 import { GetAllMedicalProceduresUseCase } from '../get-all-medical-procedures.use-case';
 
 const mockMedicalProcedureRepository: jest.Mocked<MedicalProcedureRepository> = {
@@ -89,5 +90,34 @@ describe('GetAllMedicalProceduresUseCase', () => {
     expect(mockMedicalProcedureRepository.findAll).toHaveBeenCalledWith(undefined);
     expect(result).toHaveLength(0);
     expect(result).toEqual([]);
+  });
+
+  it('should filter medical procedures by doctorId', async () => {
+    const filters: GetAllMedicalProceduresInputDto = { doctorId: 'doctor-1' };
+    mockMedicalProcedureRepository.findAll.mockResolvedValue([procedure1, procedure3]);
+
+    const result = await getAllMedicalProceduresUseCase.execute(filters);
+
+    expect(mockMedicalProcedureRepository.findAll).toHaveBeenCalledTimes(1);
+    expect(mockMedicalProcedureRepository.findAll).toHaveBeenCalledWith(filters);
+    expect(result).toHaveLength(2);
+    expect(result).toEqual([
+      {
+        id: '1',
+        doctorId: 'doctor-1',
+        patientId: 'patient-1',
+        procedureDate: procedure1.procedureDate,
+        procedureValue: 100,
+        paymentStatus: PaymentStatus.PAID,
+      },
+      {
+        id: '3',
+        doctorId: 'doctor-1',
+        patientId: 'patient-3',
+        procedureDate: procedure3.procedureDate,
+        procedureValue: 150,
+        paymentStatus: PaymentStatus.GLOSSED,
+      },
+    ]);
   });
 });
