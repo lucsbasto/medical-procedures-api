eb5fc88a3aaeafa6248b32539c4df5a0d607fcdf
Lucas Bastos <lucsbasto@gmail.com>
2025-04-11 15:53:04 -0300

    feat: add MedicalProcedureRepository interface for managing medical procedures

---
+++ src/domain/medical-procedure/repositories/medical-procedure.repository.ts
@@ -0,0 +1,9 @@
+import { MedicalProcedure } from '../entities/medical-procedure.entity';
+
+export interface MedicalProcedureRepository {
+  create(medicalProcedure: MedicalProcedure): Promise<MedicalProcedure>;
+  findById(id: string): Promise<MedicalProcedure | null>;
+  findAll(filters?: { [key: string]: any }): Promise<MedicalProcedure[]>;
+  update(medicalProcedure: MedicalProcedure): Promise<void>;
+  delete(id: string): Promise<void>;
+}
