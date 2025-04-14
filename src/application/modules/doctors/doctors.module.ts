b09be9da152bcc38a385c1f420ada74320519000
Lucas Bastos <lucsbasto@gmail.com>
2025-04-14 10:54:53 -0300

    refactor: rename TypeOrmDoctorRepository file

--- src/application/modules/doctors/doctors.module.ts
+++ src/application/modules/doctors/doctors.module.ts
@@ -2,7 +2,7 @@ import { GetAllDoctorsUseCase } from '@/domain/doctor/use-cases/get-all-doctors/
 import { GetDoctorByIdUseCase } from '@/domain/doctor/use-cases/get-doctor-by-id/get-doctor-by-id.use-case';
 import { RegisterDoctorUseCase } from '@/domain/doctor/use-cases/register-doctor/register-doctor.use-case';
 import { DoctorEntity } from '@/infrastructure/database/typeorm/entities/doctor.entity';
-import { TypeOrmDoctorRepository } from '@/infrastructure/database/typeorm/repositories/typeorm-doctor.repository';
+import { TypeOrmDoctorRepository } from '@/infrastructure/database/typeorm/repositories/doctor.repository';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DoctorsController } from './controllers/doctors.controller';
