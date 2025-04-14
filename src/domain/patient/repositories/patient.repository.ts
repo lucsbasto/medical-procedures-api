9dc86d7825630b8796ce02ca2768f0cef3aa036e
Lucas Bastos <lucsbasto@gmail.com>
2025-04-14 12:02:37 -0300

    fix: update return type of update method in PatientRepository to Promise<Patient | null>

--- src/domain/patient/repositories/patient.repository.ts
+++ src/domain/patient/repositories/patient.repository.ts
@@ -4,6 +4,6 @@ export interface PatientRepository {
   create(patient: Patient): Promise<Patient>;
   findById(id: string): Promise<Patient | null>;
   findAll(filters?: { [key: string]: any }): Promise<Patient[]>;
-  update(doctor: Patient): Promise<void>;
+  update(doctor: Patient): Promise<Patient | null>;
   delete(id: string): Promise<void>;
 }
